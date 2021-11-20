51b5ca810bef5d2ec28eb2b7aff212940a4d8706
I-LoveCpp <90536015+I-LoveCpp@users.noreply.github.com>
2021-11-20 19:24:26 +0800

    Add files via upload

--- code/main.cpp
+++ code/main.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <fstream>
 #include <windows.h>
 #include <conio.h>
 #include <thread>
@@ -6,8 +7,10 @@
 using namespace std;
 
 #pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" ) 
-#define edition "GHSL2FJDK 1.0"
+#define edition "GHSL2FJDK 2.0"
 #define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
+
+int mode = 1;
 
 void KD_H()
 {
@@ -16,17 +19,15 @@ void KD_H()
 	{
 		if (KEY_DOWN('H'))
 		{
-			if (press == 0) keybd_event('J', 0, 0, 0);
+			if (press == 0 && mode == 0) keybd_event(VK_RIGHT, 0, 0, 0);
+			if (press == 0 && mode == 1) keybd_event('J', 0, 0, 0);
 			press = 1;
 		}
 		else
 		{
-			if (press == 1)
-			{
-				keybd_event('J', 0, KEYEVENTF_KEYUP, 0);
-			}
+			if (press == 1 && mode == 0) keybd_event(VK_RIGHT, 0, KEYEVENTF_KEYUP, 0);
+			if (press == 1 && mode == 1) keybd_event('J', 0, KEYEVENTF_KEYUP, 0);
 			press = 0;
-
 		}
 		Sleep(1);
 	}
@@ -39,17 +40,15 @@ void KD_G()
 	{
 		if (KEY_DOWN('G'))
 		{
-			if (press == 0) keybd_event('F', 0, 0, 0);
+			if (press == 0 && mode == 0) keybd_event(VK_DOWN, 0, 0, 0);
+			if (press == 0 && mode == 1) keybd_event('F', 0, 0, 0);
 			press = 1;
 		}
 		else
 		{
-			if (press == 1)
-			{
-				keybd_event('F', 0, KEYEVENTF_KEYUP, 0);
-			}
+			if (press == 1 && mode == 0) keybd_event(VK_DOWN, 0, KEYEVENTF_KEYUP, 0);
+			if (press == 1 && mode == 1) keybd_event('F', 0, KEYEVENTF_KEYUP, 0);
 			press = 0;
-
 		}
 		Sleep(1);
 	}
@@ -62,17 +61,15 @@ void KD_L()
 	{
 		if (KEY_DOWN('L'))
 		{
-			if (press == 0) keybd_event('K', 0, 0, 0);
+			if (press == 0 && mode == 0) keybd_event(VK_LEFT, 0, 0, 0);
+			if (press == 0 && mode == 1) keybd_event('K', 0, 0, 0);
 			press = 1;
 		}
 		else
 		{
-			if (press == 1)
-			{
-				keybd_event('K', 0, KEYEVENTF_KEYUP, 0);
-			}
+			if (press == 1 && mode == 0) keybd_event(VK_LEFT, 0, KEYEVENTF_KEYUP, 0);
+			if (press == 1 && mode == 1) keybd_event('K', 0, KEYEVENTF_KEYUP, 0);
 			press = 0;
-
 		}
 		Sleep(1);
 	}
@@ -84,17 +81,15 @@ void KD_S()
 	{
 		if (KEY_DOWN('S'))
 		{
-			if (press == 0) keybd_event('D', 0, 0, 0);
+			if (press == 0 && mode == 0) keybd_event(VK_UP, 0, 0, 0);
+			if (press == 0 && mode == 1) keybd_event('D', 0, 0, 0);
 			press = 1;
 		}
 		else
 		{
-			if (press == 1)
-			{
-				keybd_event('D', 0, KEYEVENTF_KEYUP, 0);
-			}
+			if (press == 1 && mode == 0) keybd_event(VK_UP, 0, KEYEVENTF_KEYUP, 0);
+			if (press == 1 && mode == 1) keybd_event('D', 0, KEYEVENTF_KEYUP, 0);
 			press = 0;
-
 		}
 		Sleep(1);
 	}
@@ -105,18 +100,50 @@ void ask()
 	if (MessageBoxA(0, "点击 \"确定\" 来退出本程序", edition, MB_OK) == 1) exit(0);
 }
 
+void switch_mode()
+{
+	while (1)
+	{
+		if (KEY_DOWN(VK_MENU))
+		{
+			mode = (mode + 1) % 2;
+			Sleep(100);
+		}
+		Sleep(1);
+	}
+}
+
+void save()
+{
+	ofstream out("D:\\Data\\GHSL2FJDK\\Mode.Ceh", ios::binary);
+	out << mode;
+}
+
 int main()
 {
+	ifstream in("D:\\Data\\GHSL2FJDK\\Mode.Ceh", ios::binary);
+	if (!in)
+	{
+		CreateDirectory("D:\\Data\\", NULL);
+		SetFileAttributes("D:\\Data\\", FILE_ATTRIBUTE_HIDDEN);
+		CreateDirectory("D:\\Data\\GHSL2FJDK\\", NULL);
+		ofstream out("D:\\Data\\GHSL2FJDK\\Mode.Ceh", ios::binary);
+		out << "1";
+	}
+	in >> mode;
+	atexit(save);
 	thread task01(KD_H);
 	thread task02(KD_G);
 	thread task03(KD_L);
 	thread task04(KD_S);
 	thread task05(ask);
+	thread task06(switch_mode);
 	task01.join();
 	task02.join();
 	task03.join();
 	task04.join();
 	task05.join();
+	task06.join();
 
 	return 0;
 }
