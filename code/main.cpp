87806b5abb762610a9fc646be51d5e618932cb24
I-LoveCpp <90536015+I-LoveCpp@users.noreply.github.com>
2022-05-01 21:48:18 +0800

    Add files via upload

--- code/main.cpp
+++ code/main.cpp
@@ -1,125 +1,61 @@
 #include <cstring>
+#include <vector>
+#include <thread> 
 #include <fstream>
 #include <windows.h>
 #include <conio.h>
-#include <thread>
 
 using namespace std;
 
 #pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" ) 
-#define edition "GHSL2FJDK 2.0"
+#define edition "GHSL2FJDK 2.1"
 #define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
 
-int mode = 1;
+vector<thread> threads;
+int mode = 1, codes[6] = { 1, 2, 0, 0, 0, 0 };
+char xs[6] = { ' ', ' ', 'G', 'H', 'S', 'L' }, ys[6][2] = { { ' ', ' '}, { ' ', ' '}, {VK_DOWN, 'F'}, {VK_RIGHT, 'J'}, {VK_UP, 'D'}, {VK_LEFT, 'K'} };
 
-void KD_H()
+void task(char x, char y[2], int code) noexcept
 {
-	int press = 0;
-	while (1)
+	if (code == 0)
 	{
-		if (KEY_DOWN('H'))
+		int press = 0;
+		while (1)
 		{
-			if (press == 0 && mode == 0) keybd_event(VK_RIGHT, 0, 0, 0);
-			if (press == 0 && mode == 1) keybd_event('J', 0, 0, 0);
-			press = 1;
+			if (KEY_DOWN(x))
+			{
+				if (press == 0 && mode == 0) keybd_event(y[0], 0, 0, 0);
+				if (press == 0 && mode == 1) keybd_event(y[1], 0, 0, 0);
+				press = 1;
+			}
+			else
+			{
+				if (press == 1 && mode == 0) keybd_event(y[0], 0, KEYEVENTF_KEYUP, 0);
+				if (press == 1 && mode == 1) keybd_event(y[1], 0, KEYEVENTF_KEYUP, 0);
+				press = 0;
+			}
+			Sleep(1);
 		}
-		else
+	}
+	else if (code == 1)
+	{
+		if (MessageBoxA(0, "点击 \"确定\" 来退出本程序", edition, MB_OK) == 1) exit(0);
+	}
+	else if (code == 2)
+	{
+		while (1)
 		{
-			if (press == 1 && mode == 0) keybd_event(VK_RIGHT, 0, KEYEVENTF_KEYUP, 0);
-			if (press == 1 && mode == 1) keybd_event('J', 0, KEYEVENTF_KEYUP, 0);
-			press = 0;
+			if (KEY_DOWN(VK_MENU))
+			{
+				mode = (mode + 1) % 2;
+				Sleep(100);
+			}
+			Sleep(1);
 		}
-		Sleep(1);
 	}
 }
 
-void KD_G()
-{
-	int press = 0;
-	while (1)
-	{
-		if (KEY_DOWN('G'))
-		{
-			if (press == 0 && mode == 0) keybd_event(VK_DOWN, 0, 0, 0);
-			if (press == 0 && mode == 1) keybd_event('F', 0, 0, 0);
-			press = 1;
-		}
-		else
-		{
-			if (press == 1 && mode == 0) keybd_event(VK_DOWN, 0, KEYEVENTF_KEYUP, 0);
-			if (press == 1 && mode == 1) keybd_event('F', 0, KEYEVENTF_KEYUP, 0);
-			press = 0;
-		}
-		Sleep(1);
-	}
-}
-
-void KD_L()
-{
-	int press = 0;
-	while (1)
-	{
-		if (KEY_DOWN('L'))
-		{
-			if (press == 0 && mode == 0) keybd_event(VK_LEFT, 0, 0, 0);
-			if (press == 0 && mode == 1) keybd_event('K', 0, 0, 0);
-			press = 1;
-		}
-		else
-		{
-			if (press == 1 && mode == 0) keybd_event(VK_LEFT, 0, KEYEVENTF_KEYUP, 0);
-			if (press == 1 && mode == 1) keybd_event('K', 0, KEYEVENTF_KEYUP, 0);
-			press = 0;
-		}
-		Sleep(1);
-	}
-}
-void KD_S()
-{
-	int press = 0;
-	while (1)
-	{
-		if (KEY_DOWN('S'))
-		{
-			if (press == 0 && mode == 0) keybd_event(VK_UP, 0, 0, 0);
-			if (press == 0 && mode == 1) keybd_event('D', 0, 0, 0);
-			press = 1;
-		}
-		else
-		{
-			if (press == 1 && mode == 0) keybd_event(VK_UP, 0, KEYEVENTF_KEYUP, 0);
-			if (press == 1 && mode == 1) keybd_event('D', 0, KEYEVENTF_KEYUP, 0);
-			press = 0;
-		}
-		Sleep(1);
-	}
-}
-
-void ask()
-{
-	if (MessageBoxA(0, "点击 \"确定\" 来退出本程序", edition, MB_OK) == 1) exit(0);
-}
-
-void switch_mode()
-{
-	while (1)
-	{
-		if (KEY_DOWN(VK_MENU))
-		{
-			mode = (mode + 1) % 2;
-			Sleep(100);
-		}
-		Sleep(1);
-	}
-}
-
-void save()
-{
-	ofstream out("D:\\Data\\GHSL2FJDK\\Mode.Ceh", ios::binary);
-	out << mode;
-}
-
-int main()
+void init()
 {
 	ifstream in("D:\\Data\\GHSL2FJDK\\Mode.Ceh", ios::binary);
 	if (!in)
@@ -131,19 +67,15 @@ int main()
 		out << "1";
 	}
 	in >> mode;
-	atexit(save);
-	thread task01(KD_H);
-	thread task02(KD_G);
-	thread task03(KD_L);
-	thread task04(KD_S);
-	thread task05(ask);
-	thread task06(switch_mode);
-	task01.join();
-	task02.join();
-	task03.join();
-	task04.join();
-	task05.join();
-	task06.join();
+	atexit([]() {ofstream out("D:\\Data\\GHSL2FJDK\\Mode.Ceh", ios::binary); out << mode; });
+}
 
+int main()
+{
+	init();
+	for (int i = 0; i < 6; i++)
+		threads.push_back(thread(task, xs[i], ys[i], codes[i]));
+	for (auto it = threads.begin(); it != threads.end(); it++)
+		it->join();
 	return 0;
 }
